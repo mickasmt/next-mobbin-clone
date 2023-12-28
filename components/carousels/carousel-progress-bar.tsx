5d35af8c32c17bfa2fd6b3cc724fdd2c4745145d
mickasmt <sermont.mickael@hotmail.fr>
2023-12-28 13:28:27 +0100

    feat: add carousel progressbar

---
+++ components/carousels/carousel-progress-bar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CarouselProgressBar() {
+  return (
+    <section className="relative h-[420px] w-full bg-red-300 overflow-hidden border-b-2 border-divider-primary text-center md:h-[480px]">
+      CarouselProgressBar
+    </section>
+  );
+}
