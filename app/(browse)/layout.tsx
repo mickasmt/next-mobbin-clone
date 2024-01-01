82afe45c619f413cf293968a3bcf0890a2372516
mickasmt <sermont.mickael@hotmail.fr>
2024-01-01 22:06:34 +0100

    feat: add static hero for homepage

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,3 +1,4 @@
+import { HeroHomepage } from "@/components/hero-homepage";
 import { Navbar } from "./_components/navbar";
 import { CarouselProgressBar } from "@/components/carousels/carousel-progress-bar";
 
@@ -6,6 +7,7 @@ const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <CarouselProgressBar />
+      {/* <HeroHomepage /> */}
       <main className="flex-1">{children}</main>
 
       {/* <Footer /> */}
