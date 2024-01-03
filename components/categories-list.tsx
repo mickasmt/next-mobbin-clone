4bd28edb90918cca46c1a24052053c650adc9775
mickasmt <sermont.mickael@hotmail.fr>
2024-01-03 13:03:00 +0100

    feat: add badge in categories list

--- components/categories-list.tsx
+++ components/categories-list.tsx
@@ -3,12 +3,19 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
+import { Badge } from "./ui/badge";
 
-const links = [
+interface LinksProps {
+  name: string;
+  path: string;
+  badge?: string;
+}
+
+const links: LinksProps[] = [
   { name: "Apps", path: "/apps" },
   { name: "Screens", path: "/screens" },
   { name: "UI Elements", path: "/ui-elements" },
-  { name: "Flows", path: "/flows" },
+  { name: "Flows", path: "/flows", badge: "PRO" },
 ];
 
 export function CategoriesList() {
@@ -22,13 +29,14 @@ export function CategoriesList() {
           key={link.path}
           href={`/browse/${params.platform + link.path}`}
           className={cn(
-            "whitespace-nowrap py-1 border-b-2 transition-colors ease-out focus-visible:ring-4 focus-visible:ring-ring",
+            "flex items-center gap-x-2 whitespace-nowrap py-1 border-b-2 transition-colors ease-out focus-visible:ring-4 focus-visible:ring-ring",
             pathname === `/browse/${params.platform + link.path}`
               ? "border-primary text-primary"
               : "border-transparent text-muted-foreground hover:text-primary"
           )}
         >
-          {link.name}
+          <span>{link.name}</span>
+          {link.badge ? <Badge className="px-2 font-medium uppercase border-none">{link.badge}</Badge> : ""}
         </Link>
       ))}
     </div>
